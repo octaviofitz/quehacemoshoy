14bc087e77619a67b78091122e7395a5ac2d4887
octaviofitz <octaviofitz@gmail.com>
2022-12-06 19:21:11 -0300

    css evento

--- src/Components/Botones/Botones.js
+++ src/Components/Botones/Botones.js
@@ -10,7 +10,6 @@ function Botones() {
           <Button className='botonIndividual' id='segundoBoton' size='lg'><Link to='/semana'>Esta semana</Link></Button>
           <Button className='botonIndividual' id='tercerBoton' size='lg'><Link to='/mes'>Este mes</Link></Button>
         <Button className='botonIndividual' id='cuartoBoton' size='lg'><Link to='todos'> Todas las fechas</Link></Button>
-
         </section>
     );
 }
--- src/Components/Eventos/DetalleEvento/DetalleEvento.js
+++ src/Components/Eventos/DetalleEvento/DetalleEvento.js
@@ -1,15 +1,25 @@
-//react
-import { Link } from 'react-router-dom'
-import React, { useState } from 'react'
+import React from 'react'
+
+/* Css */
+import '../DetalleEvento/detalleEvento.css';
 
 function DetalleEvento({data}) {
 
     return (
-      <>
-           <h2>{data.evento}</h2>
-           <p>{data.lugar}</p>
+      <section className='detalleEvento'>   
+            <h3 className='titulo'>{data.evento}</h3>
+            <p className='lugar'>Se presentan en {data.lugar}</p>
 
-      </>
+           <img src={data.img} className='imagen' alt='Imagen Evento' />
+          <a href={data.link} target='_blank'><button class="button-53" role="button">Conseguir entradas</button></a>
+          <p className='descripcion'>{data.descripcion}</p>
+          <div className='informacionContainer'>
+          <h4 className='titulo'>Información del lugar</h4>
+          <p className='data'>Ciudad Cultural Konex es una organización que tiene como misión crear experiencias artísticas innovadoras. Desarrolla múltiples espectáculos y contenidos artísticos con el objetivo de acercar a la comunidad una propuesta cultural diversa y de calidad, promoviendo la expresión, la inspiración, el disfrute y la creatividad.
+          El espacio, situado en el mítico barrio del Abasto de la ciudad de Buenos Aires, cuenta con seis salas y un patio descubierto para la realización de todo tipo de eventos culturales.</p>
+          </div>
+           
+      </section>
     )
   }
   
